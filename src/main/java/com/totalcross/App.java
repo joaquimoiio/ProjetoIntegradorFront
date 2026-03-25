86ae71249adf2e17e5e70022813221beecf6c422
joaquimoiio <joaquimpaes01@gmail.com>
2026-03-25 14:46:27 -0300

    Iniciando projeto TotalCross

---
+++ src/main/java/com/totalcross/App.java
@@ -0,0 +1,14 @@
+package com.totalcross;
+import totalcross.sys.Settings;
+import totalcross.ui.MainWindow;
+public class App extends MainWindow {
+    
+    public App() {
+        setUIStyle(Settings.MATERIAL_UI);
+    }
+
+    @Override
+    public void initUI() {
+
+    }
+}
