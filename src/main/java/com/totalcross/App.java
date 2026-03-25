d20457f21fa9018fc866c738aa67fe6d2c1eeae6
joaquimoiio <joaquimpaes01@gmail.com>
2026-03-25 15:27:04 -0300

    Tela de login feita, faltar fazer função

--- src/main/java/com/totalcross/App.java
+++ src/main/java/com/totalcross/App.java
@@ -1,14 +1,40 @@
 package com.totalcross;
+
 import totalcross.sys.Settings;
+import totalcross.ui.Button;
+import totalcross.ui.Edit;
+import totalcross.ui.Label;
 import totalcross.ui.MainWindow;
+import totalcross.ui.font.Font;
+import totalcross.ui.gfx.Color;
 public class App extends MainWindow {
-    
-    public App() {
+
+	private Button btnLogin;
+	private Edit login, senha;
+
+	public App() {
         setUIStyle(Settings.MATERIAL_UI);
     }
 
     @Override
-    public void initUI() {
+	public void initUI() {
 
+		Label lblLogin = new Label("Digite seu usuario");
+		add(lblLogin, LEFT, CENTER - 125);
+
+		login = new Edit();
+		add(login, LEFT, AFTER + 5, FILL, PREFERRED);
+
+		Label lblSenha = new Label("Digite sua senha");
+		add(lblSenha, LEFT, AFTER + 10);
+
+		senha = new Edit();
+		add(senha, LEFT, AFTER + 5, FILL, PREFERRED);
+
+		btnLogin = new Button("Login");
+		btnLogin.setBackForeColors(Color.BLACK, Color.WHITE);
+		Font font = MainWindow.getDefaultFont().percentBy(150);
+		btnLogin.setFont(font);
+		add(btnLogin, CENTER, AFTER + 20, DP + 100, DP + 70);
     }
 }
